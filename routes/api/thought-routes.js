24f96f3c26a93dd0d8eb52b883504db018b69a1e
Michael Andrews <michael.andrews@pega.com>
2022-10-04 02:52:59 -0400

    started creating routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} = reqire('../../')
